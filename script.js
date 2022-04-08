7b631e21b25ff312a0909c5293a3dffd560a1629
Laurie McLaughlin <lnmclaughlin@gmail.com>
2022-04-08 11:45:48 -0400

    updated grammar, etc

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 let user= prompt("Welcome to GC mini golf! What is your name?");
 console.log(user);
 
-let count= prompt("Hi, " + user + "!" + " Would you like to play 3 or 6 holes?");
-console.log(count);
+let count= Number (prompt("Hi, " + user + "!" + " Would you like to play 3 or 6 holes?"));
 
 let total= 0;
+let putt ; 
 
-for (let i=1; i < count; i ++) {
-let putt = prompt("How many putts for hole " + i + "?" + "(par is 3)");
+for (let i=1; i <= count; i ++) {
+putt= Number(prompt("How many putts for hole " + i + "?" + " (par is 3)"));
 total += putt;
 }
+console.log(total ); 
 
-if (putt === 3 , total > 9) {
-console.log("Nice try, " + user + "Your total par was" + total + " +(par)");
-}
-else if (putt=== 3, total < 9) {
-console.log("Great job,  " + user + "!" + "Your total par was" + total + " -(par)");
+if (count == 3 && total > 9) {
+console.log("Nice try, " + user + "..." + " Your total par was " + (9-total) + " +(par).");
 }
 
-else (putt ===3, total === 3);
-console.log ("Good game,  " + user + "Your total par was 0.");
-
-if (putt === 6, total > 18) {
-console.log("Nice try, " + user + "Your total par was" + total + " +(par)");
+else if (count == 3 && total < 9) {
+console.log("Great job, " + user + "!" + " Your total par was " + (9-total) + " -(par).");
 }
-else if (putt === 6, total < 18) {
-console.log("Great job,  " + user + "!" + "Your total par was" + total + " -(par)");
+
+else if (count == 6 && total > 18) {
+console.log("Nice try, " + user + "..." + "Your total par was " + (18-total) + " +(par).");
+}
+else if (count == 6 && total < 18) {
+console.log("Great job, " + user + "!" + " Your total par was " + (18-total) + " -(par).");
 
 }
-else (putt=== 6, total === 18);
-console.log ("Good game,  " + user + "Your total par was 0.");
+else if  (count ==3 && total === 9){
+console.log ("Good game, " + user + "."+ " Your total par was 0.");
+}
+
+else if (count== 6 && total === 18){
+console.log ("Good game, " + user + "." + "Your total par was 0.");
+}
